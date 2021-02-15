feb26f5da29eb6e1d9b115d78be1abddfd0140d2
Derek-94 <ghdtjrrl94@gmail.com>
2021-02-15 13:04:07 +0900

    feat(weather): Update weather component
    - Update weather component.
    - Shows temperature infos, humidity, local name.

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,23 @@
 import React from "react" 
 
 export default class Weather extends React.Component {
+
     render() {
         return (
             <div>
-                <p>Weather information renders here. </p>
-                {this.props.lat}, {this.props.lon} 's weather is...
+                {this.props.data === null ? (
+                    <div>
+                        Coords info not loaded...
+                    </div>
+                ) : (
+                    <div>
+                        {console.log(this.props.data)}
+                        <p>🌡️ in {this.props.data.name} is {this.props.data.main.temp} celsius.</p>
+                        <p>Feels like {this.props.data.main.feels_like} celsius.</p>
+                        <p>Today 🌡️ max : {this.props.data.main.temp_max} celsius , min : {this.props.data.main.temp_min} celsius.</p>
+                        <p>💦 Humidity : {this.props.data.main.humidity}%</p>
+                    </div>
+                )}
             </div>
         );
     }
