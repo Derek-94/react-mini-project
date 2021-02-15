b5431fe5b8c4ccfa156cbc42ab90a6d4078703c9
Derek-94 <ghdtjrrl94@gmail.com>
2021-02-15 13:02:51 +0900

    feat(loaction): Update rendering
    - Make rendering using api response.

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,11 +1,19 @@
 import React from "react"
 
-const API_KEYS = "5c98c6954d5565acfa116b5902da6602";
-
 export default class Location extends React.Component {
     render() {
-        return <div>
-            You are in ({this.props.lat}, {this.props.lon})
+        return (
+        <div>
+            {this.props.data === null ? (
+                <div>
+                Not loaded...
+                </div>
+            ) : (
+                <div>
+                You are in ({this.props.lat}, {this.props.lon}), {this.props.data.name}
+                </div>
+            )}
         </div>
+        );
     }
 }
