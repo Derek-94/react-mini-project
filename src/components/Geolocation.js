2f723c0cdff2545ef130d2cde9e752b051f239b6
Derek-94 <ghdtjrrl94@gmail.com>
2021-02-15 12:58:49 +0900

    feat(geoloaction): Update api works

--- src/components/Geolocation.js
+++ src/components/Geolocation.js
@@ -2,27 +2,35 @@ import React from "react"
 import Location from "./Location"
 import Weather from "./Weather"
 
+const API_key = "8f165c303e16656f72e24f834ab6fb7b";
+
 export default class Geolocation extends React.Component {
     state = {
         isLoading : true,
-        longtitude : 0,
-        latitude : 0
+        longitude : 0,
+        latitude : 0,
+        data : null
     }
 
-    async componentDidMount() {
+    componentDidMount() {
         setTimeout( () => {
             if(navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition( (pos) => {
+                navigator.geolocation.getCurrentPosition( async(pos) => {
                     this.setState({
                         isLoading : false,
-                        longitude : pos.coords.longitude,
-                        latitude : pos.coords.latitude
+                        longitude : pos.coords.longitude.toFixed(5),
+                        latitude : pos.coords.latitude.toFixed(5)
                     })
-                } )
+
+                    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&appid=${API_key}&units=metric`);
+                    const responseData = await response.json();
+                    this.setState({data : responseData}) 
+                }) 
             } else {
                 alert("Doesn't run geolocation function.")
             } 
-        }, 2000 )      
+        }, 2000)
+        
     }
 
     render () {
@@ -37,10 +45,12 @@ export default class Geolocation extends React.Component {
                     <Location 
                         lon = {this.state.longitude}
                         lat = {this.state.latitude}
+                        data = {this.state.data}
                     />
                     <Weather 
                         lon = {this.state.longitude}
                         lat = {this.state.latitude}
+                        data = {this.state.data}
                     />
                 </div>
             )}
